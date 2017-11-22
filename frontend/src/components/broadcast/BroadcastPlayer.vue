d27c3b1b5060c388133f6dd589979b7c25c742d1
puzzledPublic <puzzledPublic@gmail.com>
2017-11-22 23:10:18 +0900

    videojs contrib hls

--- frontend/src/components/broadcast/BroadcastPlayer.vue
+++ frontend/src/components/broadcast/BroadcastPlayer.vue
@@ -1,15 +1,17 @@
 <template>
     <div class="column is-9">
         <div class="videoPlayer">
-            <video :id="videoId" class="video-js">
+            <video :id="videoId" class="video-js vjs-16-9 vjs-big-play-centered" data-setup='{}'>
             </video>
         </div>
     </div>
 </template>
 
 <script>
-import videojs from 'video.js'
+//videojs-contrib-hls 모듈이 window에 있는 videojs와 자동 결합하는듯? 그래서 webpack설정에서 videojs를 window에 먼저 로드한다. 
+const videojs = window.videojs //import videojs from 'video.js' 
 import videocss from 'video.js/dist/video-js.css'
+import 'videojs-contrib-hls/dist/videojs-contrib-hls.js'
 
 export default {
     name: 'broadcastPage',
@@ -22,8 +24,11 @@ export default {
     },
     mounted() {
         if(!this.player) {
-            //this.initialize();
+            this.initialize();
         }
+    },
+    beforeDestroy() {
+        videojs(this.videoId).dispose();
     },
     methods: {
         initialize() {
@@ -31,14 +36,17 @@ export default {
                 autoplay: false,
                 controls: true,
                 preload: 'auto',
-                fluid: false,
+                fluid: true,
                 techOrder: ['html5'],
                 
             }
             this.player = videojs(this.videoId, options);
             this.player.src({
-                src: '//vjs.zencdn.net/v/oceans.mp4',
-                type: 'video/mp4'
+                //src: '//vjs.zencdn.net/v/oceans.mp4',
+                //type: 'video/mp4',
+                src: 'https://mnmedias.api.telequebec.tv/m3u8/29880.m3u8',
+                type: 'application/x-mpegURL',
+                withCredentials: false,
             })
         }
     }
@@ -47,10 +55,13 @@ export default {
 </script>
 
 <style scoped>
-.video-js {
-    position: relative;
-    width: 100%;
-    height: 475px;
+@media only screen and (max-width:800px) {
+    .column {
+        padding: 0;
+    }
+    .videoPlayer {
+        padding: 0 0.75rem;
+    }
 }
 </style>
 
