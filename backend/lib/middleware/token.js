ab542e06c7e5cc8aacdbfc5a34a91bfb36000d67
puzzledPublic <puzzledPublic@gmail.com>
2018-01-05 01:13:16 +0900

    token time

--- backend/lib/middleware/token.js
+++ backend/lib/middleware/token.js
@@ -10,9 +10,9 @@ module.exports = doAsync( async (req, res, next) => {
     const decoded = await decodeToken(token);
     const {user} = decoded;
     
-    if(Date.now() - decoded.iat * 1000 > 30 * 1000) {  //30초 이상 지난 경우 
-        const refreshToken = await createToken({user}, {subject : 'user', expiresIn: '60s'});
-        res.cookie('access_token', refreshToken, {maxAge: 30 * 1000, httpOnly: true});
+    if(Date.now() - decoded.iat * 1000 > 2 * 30 * 1000) {  //30초 이상 지난 경우 
+        const refreshToken = await createToken({user}, {subject : 'user', expiresIn: '5m'});
+        res.cookie('access_token', refreshToken, {maxAge: 5 * 30 * 1000, httpOnly: true});
     }
 
     req.user = user;
